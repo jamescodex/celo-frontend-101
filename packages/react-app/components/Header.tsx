22771f08189e932f62a00651433370a7809f735b
James <james.john.xml@gmail.com>
2023-06-18 09:37:23 +0100

    Removed Hambunger menu from mobile display

--- packages/react-app/components/Header.tsx
+++ packages/react-app/components/Header.tsx
@@ -31,19 +31,6 @@ export default function Header() {
                 </div>
               </div>
             </div>
-  
-            <Disclosure.Panel className="sm:hidden">
-              <div className="space-y-1 pt-2 pb-4">
-                <Disclosure.Button
-                  as="a"
-                  href="#"
-                  className="block border-l-4 border-black py-2 pl-3 pr-4 text-base font-medium text-black"
-                >
-                  Home
-                </Disclosure.Button>
-                {/* Add here your custom menu elements */}
-              </div>
-            </Disclosure.Panel>
           </>
         )}
       </Disclosure>
